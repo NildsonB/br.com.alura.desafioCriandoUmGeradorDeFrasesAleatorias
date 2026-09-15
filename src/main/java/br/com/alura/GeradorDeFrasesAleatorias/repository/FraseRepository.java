1e9b3114773336074f7d628562a77379a03d025f
Nildson B Andrade <bezerranildson@gmail.com>
2026-09-14 23:46:48 -0300

    Feat: Implementar FraseService

--- src/main/java/br/com/alura/GeradorDeFrasesAleatorias/repository/FraseRepository.java
+++ src/main/java/br/com/alura/GeradorDeFrasesAleatorias/repository/FraseRepository.java
@@ -2,8 +2,12 @@ package br.com.alura.GeradorDeFrasesAleatorias.repository;
 
 import br.com.alura.GeradorDeFrasesAleatorias.model.Frase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FraseRepository extends JpaRepository<Frase, Long> {
+
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
+    public Frase fraseAleatoria();
 }
